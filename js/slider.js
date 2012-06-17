f82c89c03020845e89e6709f25df1338a6cd0768
leethompson <leethompson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-17 19:47:32 +0000

    tagging version 2.3
    git-svn-id: https://plugins.svn.wordpress.org/lazy-content-slider/trunk@559497 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,12 @@
 $(document).ready(function($){
 $("#featured").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
+$("#featured").hover(
+        function() {
+            $("#featured").tabs("rotate",0,false);
+        },
+        function() {
+            $("#featured").tabs("rotate",5000,true);
+        }
+    );
+});
 
-        });   
